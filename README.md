8e954682ffceb551d532cb0272904bb87fdc49a4
Najma Abdi <anajmabdi@gmail.com>
2022-08-26 14:14:36 -0700

    clear

--- README.md
+++ README.md
@@ -2,12 +2,19 @@ Describe Pizza()
 
 Test: "It should return a Pizza object for toppings and size"
 Code: const firstPizza = new Pizza("Cheese", "large");
-Expected Output: PizzaPizza { toppings: "Cheese", size: "large" }
+Expected Output: Pizza { toppings: "Cheese", size: "large" }
 
+Describe: totalPrice()
 
 Test: "It should return the total of the pizza order"
 Code: Pizza.prototype.totalPrice()
 Expected Output: $7, $12, $13
+
+Describe: handleFormSubmission()
+
+Test: "It should return the total after user submits selections
+Code: let priceTotal(pizza)
+Expected Output: $#
 
 <!-- 
 #### Brainstorm
@@ -16,3 +23,5 @@ Expected Output: $7, $12, $13
 * Create a prototype method that will reveal cost of pizza depending on order
 
 * Add sauce, and cheese options if code works -->
+
+//figure out a way to merge pricing total if there are more than one selection in each property
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+
 // Creating Object type
 
 function Pizza(toppings, size) {
@@ -5,3 +7,64 @@ function Pizza(toppings, size) {
   this.size = size
 }
 
+// Creating price total based off of user's chosen selections
+
+Pizza.prototype.priceTotal = function() {
+  let toppingPrice = 0;
+  let sizePrice = 0;
+
+  //Topping pricing
+  if (this.toppings === "pepperoni") {
+    toppingPrice += 4;
+  }
+  if (this.toppings === "pineapple") {
+    toppingPrice += 2;
+  }
+  if (this.toppings === "veggie") {
+    toppingPrice +=2;
+  }
+  // if (this.toppings === "pepperoni" && "pineapple") {
+  //   toppingPrice +=6
+  // }
+  // if (this.toppings === "veggie" && "pineapple") {
+  //   toppingPrice +=4
+  // }
+  // if (this.toppings === "pepperoni" && "veggie") {
+  //   toppingPrice +=6
+  // }
+  // if (this.toppings === "pepperoni" && "veggie" && "pineapple") {
+  //   toppingPrice +=8
+  // }
+
+  //Size pricing
+  if (this.size === "small") {
+    sizePrice += 5;
+  }
+  if (this.size === "medium") {
+    sizePrice += 7;
+  }
+  if (this.size === "large") {
+    sizePrice += 10;
+  }
+  return toppingPrice + sizePrice;
+}
+console.log("check");
+
+
+// UI Logic
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("pizza-form");
+  form.addEventListener("submit", handleFormSubmission)
+})
+
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const form = document.getElementById("pizza-form");
+  let toppingSelections = form.toppings
+  let sizeSelection = form.size
+  console.log(toppingSelections, sizeSelection);
+
+  let pizza = new Pizza(toppingSelections, sizeSelection);
+}
